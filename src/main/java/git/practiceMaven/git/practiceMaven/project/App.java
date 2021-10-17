77058be2474cf35675c681019354d110f6afab4e
KaranBhela <bkaranpreet@gmail.com>
2021-10-16 21:08:51 -0400

    This is practice code

---
+++ src/main/java/git/practiceMaven/git/practiceMaven/project/App.java
@@ -0,0 +1,13 @@
+package git.practiceMaven.git.practiceMaven.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
